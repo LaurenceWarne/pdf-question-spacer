fd40f656a9664f43a18e40626a2c79c6463905fa
lWarne <laurencewarne@gmail.com>
2019-10-05 20:31:28 +0100

    Add setup.py, create TextRowExtractor for extracting rows of text from image files.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pdf-question-spacer",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3",
+    url="",
+    version="1.1",
+    author="Laurence Warne",
+    license="MIT",
+    install_requires=[],
+    zip_safe=False
+)
---
+++ text_row_extractors.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class TextRowExtractor():
+
+    def __init__(self, pixel_predicate=lambda arr: arr == 0):
+        self.pixel_predicate = pixel_predicate
+
+    def extract(self, image):
+        row_contains_black = np.any(self.pixel_predicate(image), axis=1)
+        runs = find_runs(1, row_contains_black)
+        return np.fromiter(image[slice(*run)] for run in runs)
+
+
+# credit:
+# https://stackoverflow.com/questions/31544129/extract-separate-non-zero-blocks-from-array
+def find_runs(value, a):
+    # Create an array that is 1 where a is `value`, and pad each end with an extra 0.
+    isvalue = np.concatenate(([0], np.equal(a, value).view(np.int8), [0]))
+    absdiff = np.abs(np.diff(isvalue))
+    # Runs start and end where absdiff is 1.
+    ranges = np.where(absdiff == 1)[0].reshape(-1, 2)
+    return ranges
